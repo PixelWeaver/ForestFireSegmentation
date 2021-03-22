ad1fd746d50e48ad6a49fbdcbaa2b38dd28ea899
Pixel Weaver <antoinecajot@gmail.com>
2021-03-22 13:09:18 +0100

    Correctly plot top10 most frequent image sizes

--- dataset.py
+++ dataset.py
@@ -9,14 +9,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def row_to_resolution(row):
-    return int(row["width"]) * int(row["height"])
-
-
-def row_to_size_string(row):
-    return f'{row["width"]}x{row["height"]}'
-
-
 def strip(st):
     return st.strip().replace('"', "")
 
@@ -29,22 +21,31 @@ class Dataset:
     def print_cdfb_statistics():
         metadata_df = pd.read_csv('corsican_fire_db/bdfire/data.csv')
         print(f'Total number of pictures: {len(metadata_df)}')
+
+        # Read every image's resolution
         df = pd.DataFrame([], columns=['width', 'height'])
         for i, row in metadata_df.iterrows():
             im = cv2.imread('corsican_fire_db/' + strip(row[' "photo"']))
             df = df.append({'width': im.shape[1], 'height': im.shape[0]}, ignore_index=True)
 
         df["resolution"] = df.apply(lambda row: int(row["width"]) * int(row["height"]), axis=1)
-        df["size_string"] = df.apply(row_to_size_string, axis=1)
+        df["size_string"] = df.apply(lambda row: f'{row["width"]}x{row["height"]}', axis=1)
 
-        # sort dataframe
+        # sort/count dataframe
         sorted_df = df.sort_values(by=['resolution', 'size_string'], ascending=[True, True])
-        counted_df = sorted_df.groupby(['size_string', 'resolution'], sort=False)["size_string"]\
+        counted_df = sorted_df.groupby(['size_string', 'resolution'], sort=False, as_index=False)["size_string"]\
             .agg(['count'])
-        print(len(counted_df))
+        print(f'Number of different sizes: {len(counted_df)}')
 
-        # Initialize the matplotlib figure
-        f, ax = plt.subplots(figsize=(15, 6))
+        # Extract top 10 counts
+        top_10 = counted_df.sort_values(by=['count'], ascending=[False]).head(10)
+
+        # Plot it
+        bars = sns.barplot(x="size_string", y="count", data=top_10.reset_index())
+        bars.set_xticklabels(top_10.reset_index()['size_string'], rotation=90)
+        plt.tight_layout()
+        plt.savefig("figures/dataset_img_size_top10")
+        plt.show()
 
     def __init__(self):
         print(2)
