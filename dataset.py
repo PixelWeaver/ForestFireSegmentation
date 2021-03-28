d8c99b81af9427fb93870b8b9a2bdea770c6f8bd
Pixel Weaver <antoinecajot@gmail.com>
2021-03-28 03:55:52 +0200

    Added predefined split

--- dataset.py
+++ dataset.py
@@ -2,6 +2,9 @@ import seaborn as sns
 import cv2
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import os
+import numpy as np
 
 
 def strip(st):
@@ -9,8 +12,35 @@ def strip(st):
 
 
 class Dataset:
-    def parse_cfdb(self):
-        print("corsican_fire_db contains")
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def generate_rgb_split():
+        metadata_df = pd.read_csv('corsican_fire_db/bdfire/data.csv')
+
+        # Push every input image name in a list
+        X = []
+        for i, row in metadata_df.iterrows():
+            X.append(strip(row[' "photo"']))
+
+        # Push every output (GT) image name in a list
+        y = []
+        for i, row in metadata_df.iterrows():
+            y.append(strip(row[' "verite_terrain"']))
+
+        # Split & shuffle it
+        # random_state for reproducibility
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1319181)
+        X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=1319181)
+
+        if not os.path.isdir("split"):
+            os.mkdir("split")
+
+        np.savez_compressed("split/train.npz", X=X_train, y=y_train)
+        np.savez_compressed("split/val.npz", X=X_val, y=y_val)
+        np.savez_compressed("split/test.npz", X=X_test, y=y_test)
+        print(f'Splits generated')
 
     @staticmethod
     def print_cdfb_statistics():
@@ -43,5 +73,3 @@ class Dataset:
         plt.savefig("figures/dataset_img_size_top10")
         plt.show()
 
-    def __init__(self):
-        print(2)
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from dataset import Dataset
-from workbench import Workbench
 
 if __name__ == '__main__':
-    Dataset.print_cdfb_statistics()
+    Dataset.generate_rgb_split()
 
 
