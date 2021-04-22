fa606e9b5be425089172952c3a42f2d60487366b
Pixel Weaver <antoinecajot@gmail.com>
2021-04-23 01:06:01 +0200

    added plot history function

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
+import json
 import math
+import os
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 def get_venn_circle_height(num):
     baseHeight = 20
@@ -25,3 +30,34 @@ def load_row(row):
 
         return cv2.imread(rgb_path), np.expand_dims(cv2.imread(gt_path, flags=cv2.IMREAD_GRAYSCALE), axis=2)/255, nir_im 
 
+def plot_history(name, plot_val=True):
+    with open(f'histories/{name}.json', 'r') as fp:
+            dir_list = [
+                "figures",
+                f"figures/{name}"
+            ]
+
+            for path in dir_list:
+                if not os.path.isdir(path):
+                    os.mkdir(path)
+
+            data = json.load(fp)
+
+            df = pd.DataFrame()
+            for key in data.keys():
+                df[key] = pd.Series(data[key])
+
+            keys = data.keys()
+            if plot_val:
+                keys = list(keys)[:int(len(keys)/2)]
+
+            for key in keys:
+                columns = [key]
+                if plot_val:
+                    columns.append(f"val_{key}")
+                sns.lineplot(data=df[columns], palette="tab10", linewidth=2.5)
+                plt.tight_layout()
+                plt.savefig(f"figures/{name}/{key}")
+                plt.figure()
+
+plot_history("flame_unet_1")
