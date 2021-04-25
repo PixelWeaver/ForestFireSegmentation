1f00ff95607d624f9edb6de7e38f94ea67bcbd11
Pixel Weaver <antoinecajot@gmail.com>
2021-04-25 16:12:10 +0200

    testing only

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ if __name__ == '__main__':
     p = Parameters.from_file("flame_unet_1")
     d = Dataset(p)
     m = UNetModel(p)
-    m.build(tf.keras.optimizers.Adam)
-    m.train(d,
-        include_val=True,
-        save_history=True,
-        save_model=True
-    )
+    # m.build(tf.keras.optimizers.Adam)
+    # m.train(d,
+    #     include_val=True,
+    #     save_history=True,
+    #     save_model=True
+    # )
+    m.load_trained()
     m.test(d)
