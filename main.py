b83d1bbad426fe06523b6b3c99c46e65e5bab1a3
Pixel Weaver <antoinecajot@gmail.com>
2021-05-03 13:36:47 +0200

    pred test

--- main.py
+++ main.py
@@ -8,7 +8,5 @@ if __name__ == '__main__':
     p = Parameters.from_file("flame_unet_1")
     d = Dataset(p)
     m = UNetModel(p)
-    m.train(
-        d
-    )
+    m.load_trained()
     m.prediction_test(d)
