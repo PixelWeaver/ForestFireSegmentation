dd712f5f385c25a86e1451f8aa2d3e6ac1b3c1f1
Pixel Weaver <antoinecajot@gmail.com>
2021-05-08 18:34:29 +0200

    removing unused import

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from dataset import Dataset
 from parameters import Parameters
 from utils import *
 from models import *
-import tensorflow as tf
 
 if __name__ == '__main__':
     p = Parameters.from_file("deeplab_v3")
