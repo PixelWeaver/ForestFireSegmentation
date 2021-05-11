0eb222447b0ef717118c9332ac2683a115a7bded
Pixel Weaver <antoinecajot@gmail.com>
2021-05-11 17:15:17 +0200

    removing hardcoded plot

--- utils.py
+++ utils.py
@@ -61,5 +61,3 @@ def plot_history(name, plot_val=True):
                 plt.tight_layout()
                 plt.savefig(f"figures/{name}/{key}")
                 plt.figure()
-
-plot_history("deeplab_v3")
