c8358b8b5ed1d91bcbdf40c4f718b3e0f4738913
Starkhurst <47823455+Starkhurst@users.noreply.github.com>
2021-05-15 02:08:21 +0200

    adding ability to compare different trainings + code cleanup

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import functools
 import json
 import math
 import os
@@ -30,17 +31,20 @@ def load_row(row):
 
         return (cv2.imread(rgb_path), np.expand_dims(cv2.imread(gt_path, flags=cv2.IMREAD_GRAYSCALE), axis=2)/255, nir_im)
 
+def ensure_folders_exist(paths):
+    for path in paths:
+        if not os.path.isdir(path):
+            os.mkdir(path)
+
 def plot_history(name, plot_val=True):
+    # Ensure output folder existence
+    ensure_folders_exist([
+        "figures",
+        f"figures/{name}"
+    ])
+
+    # Parse and plot all metrics
     with open(f'histories/{name}.json', 'r') as fp:
-            dir_list = [
-                "figures",
-                f"figures/{name}"
-            ]
-
-            for path in dir_list:
-                if not os.path.isdir(path):
-                    os.mkdir(path)
-
             data = json.load(fp)
 
             df = pd.DataFrame()
@@ -61,3 +65,46 @@ def plot_history(name, plot_val=True):
                 plt.tight_layout()
                 plt.savefig(f"figures/{name}/{key}")
                 plt.figure()
+
+def cmp_plot_history(names : "list[str]", plot_val=True):
+    plots = {} # there will be as many plots as there are different metrics in the history files
+
+    resulting_name = functools.reduce(lambda a, b : a + "_" + b, sorted(names)) # Compute output folder name
+
+    # Ensure folder existence
+    ensure_folders_exist([
+        "figures",
+        f"figures/cmp_{resulting_name}"
+    ])
+
+    # Parse histories and add to plot dict(key = metric)
+    for name in names:
+        with open(f'histories/{name}.json', 'r') as fp:
+                data = json.load(fp)
+
+                keys = data.keys()
+                if plot_val:
+                    keys = list(keys)[:int(len(keys)/2)]
+
+                for key in keys:
+                    # Ensure dataframe had been initialized
+                    if key not in plots:
+                        plots[key] = pd.DataFrame()
+                            
+                    # Add data to dataframe
+                    plots[key][name] = pd.Series(data[key])
+                    if plot_val:
+                        plots[key][f"val_{name}"] = pd.Series(data[f"val_{key}"])
+    
+    # Plot all metrics
+    for key in plots.keys():
+        plots[key].index = range(1,len(plots[key])+1) # Start at epoch 1
+        sns.lineplot(data=plots[key], palette="tab10", linewidth=2.5)
+        plt.tight_layout()
+        plt.savefig(f"figures/cmp_{resulting_name}/{key}")
+        plt.figure()
+
+cmp_plot_history([
+    "dlv3_efficientnet",
+    "dlv3_efficientnet_2"
+])
