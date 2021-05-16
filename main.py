302fbb6fef10a0760144c8a0e2cce2be875556db
Starkhurst <47823455+Starkhurst@users.noreply.github.com>
2021-05-16 17:13:11 +0200

    adding final dlv3_efficientnet train-test run

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from utils import *
 from models import *
 
 if __name__ == '__main__':
-    p = Parameters.from_file("dlv3_efficientnet")
+    p = Parameters.from_file("dlv3_efficientnet_opt")
     d = Dataset(p)
     m = DLV3P_EfficientNet(p)
-    m.train(d)
+    m.train(
+        d,
+        save_history=False,
+        save_model=True,
+        include_val=True)
+    m.test(d)
+    m.prediction_test
