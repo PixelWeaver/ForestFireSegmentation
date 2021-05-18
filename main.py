2e371bbfbaef21c2362cc6b1a66690ecfcfc74e0
Starkhurst <47823455+Starkhurst@users.noreply.github.com>
2021-05-18 15:40:26 +0200

    att squeeze unet opt run + test

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from utils import *
 from models import *
 
 if __name__ == '__main__':
-    p = Parameters.from_file("att_squeeze_unet")
+    p = Parameters.from_file("att_squeeze_unet_opt")
     d = Dataset(p)
     m = ATTSqueezeUNet(p)
-    m.train(d)
+    m.train(d,
+        include_val=True,
+        save_history=False,
+    )
+    m.test(d)
